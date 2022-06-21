8d2a1153c50ddcc37615abff6cf9f278be1cf04c
vasyavasy <107908130+vasyavasy@users.noreply.github.com>
2022-06-21 12:31:16 +0700

    Add files via upload

---
+++ Transport.cs
@@ -0,0 +1,196 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Worker_2
+ * Date: 21.06.2022
+ * Time: 10:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace transport
+{
+	public abstract class Transport
+        {
+            public int Year { get; set; }
+            public int Weight { get; set; }
+            public string Color { get; set; }
+
+  
+
+            protected Transport(int year, int weight, string color)
+            {
+                Year = year;
+                Weight = weight;
+                Color = color;
+            }
+
+            public abstract void Info();
+        }
+	
+	 class Train : Transport
+    {
+        public int Сarriages { get; set; }
+        public Train(int year, int weight, string color, int carriages) : base(year, weight, color)
+        {
+            Сarriages = carriages;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Поезд");
+            Console.WriteLine($"Год: {Year}\n" +
+                              $"Вес: {Weight}\n" +
+                              $"Цвет: {Color}");
+            Console.WriteLine($"Грузоподъёмность: {Сarriages}\n");
+        }
+    }
+	 
+	public class Airplane : Transport
+    {
+        public double WingLength { get; set; }
+        public Airplane(int year, int weight, string color, double wingLength) : base(year, weight, color)
+        {
+            WingLength = wingLength;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Самололёт");
+            Console.WriteLine($"Год: {Year}\n" +
+                              $"Вес: {Weight}\n" +
+                              $"Цвет: {Color}");
+            Console.WriteLine($"Грузоподъёмность: {WingLength:0.00}\n");
+        }
+    }
+	
+	
+	public class Car : Transport
+    {
+        public double Speed { get; set; }
+        public Car(int year, int weight, string color, double speed) : base(year, weight, color)
+        {
+            Speed = speed;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Машина");
+            Console.WriteLine($"Год: {Year}\n" +
+                              $"Вес: {Weight}\n" +
+                              $"Цвет: {Color}");
+            Console.WriteLine($"Скорость: {Speed:0.00}\n");
+        }
+    }
+	
+	
+	
+	 public class Car : Transport
+    {
+        public double Speed { get; set; }
+        public Car(int year, int weight, string color, double speed) : base(year, weight, color)
+        {
+            Speed = speed;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Car");
+            Console.WriteLine($"Year: {Year}\n" +
+                              $"Weight: {Weight}\n" +
+                              $"Color: {Color}");
+            Console.WriteLine($"Speed: {Speed:0.00}\n");
+        }
+    }
+	 
+	 
+	public class Truck : Car
+    {
+        public double BodyLength { get; set; }
+        public Truck(int year, int weight, string color, double speed, double bodyLength) : base(year, weight, color, speed)
+        {
+            BodyLength = bodyLength;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Truck");
+            Console.WriteLine($"Year: {Year}\n" +
+                              $"Weight: {Weight}\n" +
+                              $"Color: {Color}\n" +
+                              $"Speed: {Speed:0.00}");
+            Console.WriteLine($"BodyLength: {BodyLength:0.00}\n");
+        }
+    }
+	
+	public class Passenger : Car
+    {
+        public string PassengerType { get; set; }
+        public Passenger(int year, int weight, string color, double speed, string passengerType) : base(year, weight, color, speed)
+        {
+            PassengerType = passengerType;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Truck");
+            Console.WriteLine($"Year: {Year}\n" +
+                              $"Weight: {Weight}\n" +
+                              $"Color: {Color}\n" +
+                              $"Speed: {Speed:0.00}");
+            Console.WriteLine($"PassengerType: {PassengerType}\n");
+        }
+    }
+	
+	public class PassengerPlane : Airplane
+    {
+        public int Seats { get; set; }
+        public PassengerPlane(int year, int weight, string color, double wingLength, int seats) : base(year, weight, color, wingLength)
+        {
+            Seats = seats;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Airplane");
+            Console.WriteLine($"Year: {Year}\n" +
+                              $"Weight: {Weight}\n" +
+                              $"Color: {Color}\n" +
+                              $"WingLength: {WingLength:0.00}");
+            Console.WriteLine($"Seats: {Seats}\n");
+        }
+    }
+	
+	public class CargoPlane : Airplane
+    {
+        public double Capacity { get; set; }
+        public CargoPlane(int year, int weight, string color, double wingLength, double capacity) : base(year, weight, color, wingLength)
+        {
+            Capacity = capacity;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("Airplane");
+            Console.WriteLine($"Year: {Year}\n" +
+                              $"Weight: {Weight}\n" +
+                              $"Color: {Color}\n" +
+                              $"WingLength: {WingLength:0.00}");
+            Console.WriteLine($"Capacity: {Capacity:0.00}\n");
+        }
+    }
+	
+	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
